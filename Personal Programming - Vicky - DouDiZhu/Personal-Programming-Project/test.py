c35c098caf0cdcb5aef6783ca3c64a9b1e9200e1
Vicky <Vzhang@connect.kellettschool.com>
2026-04-15 12:52:50 +0800

    put a rank map, tried to make check-current-combo work

--- Personal Programming - Vicky - DouDiZhu/Personal-Programming-Project/test.py
+++ Personal Programming - Vicky - DouDiZhu/Personal-Programming-Project/test.py
@@ -3,6 +3,7 @@
 #WHY IS IT NOT COMMITING
 
 import random 
+from collections import Counter
 
 starting = True
 turn_count = 0
@@ -18,7 +19,10 @@ all_card_names = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9', 'h10', '
                   's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 'sJ', 'sQ', 'sK',
                   'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'd10', 'dJ', 'dQ', 'dK',
                   'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'cJ', 'cQ', 'cK',
-                  'Black_Joker', 'Red_Joker']
+                  'B!', 'R!']
+
+# Rank map
+rank_map = {'3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14, '2': 15, 'B!': 16, 'R!':17}
 
 #combined deck
 full_deck = list(zip(all_cards, all_card_names))
@@ -45,7 +49,7 @@ Airplane: at least two consecutive trios
 Airplane with small wings: at least two consecutive trio and the same number of solo cards
 Air plane with large wings: at least two consecutive trios and the same number of pairs
 Four with two: four of the same card and two solos
-Four with two pairs: four of the same card and two pairs
+Four with a pair: four of the same card and a pair
 ''')
     
     start_game = int(input("Type 1 to [Continue] \n "))
@@ -134,11 +138,13 @@ def landlord_cards():
 
 # Show cards
 def show_cards_left(player_cards):
+    sorted_hand = sorted(player_cards, key=lambda card: rank_map[card[0:]])
     print("Your cards: ", end="")
-    for i, card in enumerate(player_cards):
+    for i, card in enumerate(sorted_hand):
         print(f"{i+1}:{card[0]}:{card[1]} ", end="")
     print()
     print(f"You currently have: {len(player_cards)} cards")
+
 
 # Check if landlord is player
 def check_landlord_is_player(landlord_choice):
@@ -149,20 +155,54 @@ def check_landlord_is_player(landlord_choice):
     return player_identity
 
 def check_combination(cards_played):
-    # Placeholder 
+    # check combo types by seeing how many cards per value is played
+    lisssst = []
+    for card in cards_played:
+        lisssst.append(card[1])
+    #counting how many per value
+    counting_cards = Counter(lisssst)
+    #assigning current_combo (simple)
+    if 3 and 2 in counting_cards and len(counting_cards) == 5:
+        current_combo_type = "Trio with pair"
+    if 4 in counting_cards and len(counting_cards) == 4:
+        current_combo_type = "Bomb"
+    if 3 and 1 in counting_cards and len(counting_cards) == 5:
+        current_combo_type = "Trio with single card"
+    if 1 in counting_cards and len(counting_cards) == 1:
+        current_combo_type = "Solo"
+    if "!" and "!" in counting_cards and len(counting_cards) == 2:
+        current_combo_type = "Rocket" 
+    if "!" in counting_cards and len(counting_cards) == 1:
+        current_combo_type = "Single Joker"
+    if 4 and 1 and 1 in counting_cards and len(counting_cards) == 6:
+        current_combo_type = "Four with two"
+    if 4 and 2 in counting_cards and len(counting_cards) == 6:
+        current_combo_type = "Four with a pair"
+    
+    #assigning current_combo (in ascension)
+    #placeholder
+    
     pass
+    #return current_combo_type
+ 
+
+
 
 def landlord_play(player_identity, player_cards, bot1_cards, bot2_cards):
     if player_identity == 1:
         show_cards_left(player_cards)
-        # Add later
+        play_combination(turn_count,player_cards,current_combo_type)
+        # Add later 
 
-def play_combination(turn_count, player_cards, bot1_cards, bot2_cards, current_combo_type):
-    if turn_count == 0:
-        current_combo = input("*Please play a combination*\n")
+def play_combination(turn_count, cards, current_combo_type):
+    if turn_count%3 == 0:
+        cards_played = input("*Please play a combination*\n")
+        print("You have played:", cards_played)
+
     else:
-        current_combo = input(f"Please play cards that follow the {current_combo_type} combination\n")
+        cards_played = input(f"Please play cards that follow the {current_combo_type} combination\n")
     # Add later
+    pass
 
 #---- start of game ---
 starting = intro()
